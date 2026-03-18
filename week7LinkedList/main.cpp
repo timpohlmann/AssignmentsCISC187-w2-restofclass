8021fec5900ef1fe9922824ee8a208ff807611ee
timpohlmann <timp2006@icloud.com>
2026-03-17 18:41:48 -0700

    Update main.cpp

--- week7LinkedList/main.cpp
+++ week7LinkedList/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include "stack.h"
+#include "stack.h"//necessary
 using namespace std;
 
 int main() {
-    Stack s;
+    Stack s;//creating stack s
     cout<<s.isEmpty()<<endl;
-    s.push(10);//pushing
+    s.push(10);//pushing 3 elements
     s.push(20);
     s.push(30);
-    s.display();
+    s.display();//displaying elements currently in stack 
     cout<<endl;
-    cout<<"The value at the top of the current stack: "<<s.peek()<<endl;
+    cout<<"The value at the top of the current stack: "<<s.peek()<<endl;//showing the value at the top of the stack
 
-    s.pop();
+    s.pop();//removing an element
     cout<<"Displaying after removing(pop): ";
-    s.display();
+    s.display();//showing elements currently in stack again
 
 }
